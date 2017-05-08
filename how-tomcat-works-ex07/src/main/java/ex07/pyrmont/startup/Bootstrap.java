1caf707311266a819f259ecb647fca84fec6846c
易德军 <yidejun@9tongmail.com>
2017-05-08 16:02:05 +0800

    format

--- how-tomcat-works-ex07/src/main/java/ex07/pyrmont/startup/Bootstrap.java
+++ how-tomcat-works-ex07/src/main/java/ex07/pyrmont/startup/Bootstrap.java
@@ -16,58 +16,57 @@ import org.apache.catalina.Wrapper;
 import org.apache.catalina.connector.http.HttpConnector;
 
 public final class Bootstrap {
-  public static void main(String[] args) {
-    Connector connector = new HttpConnector();
-    
-    Wrapper wrapper1 = new SimpleWrapper();
-    wrapper1.setName("Primitive");
-    wrapper1.setServletClass("PrimitiveServlet");
-    
-    Wrapper wrapper2 = new SimpleWrapper();
-    wrapper2.setName("Modern");
-    wrapper2.setServletClass("ModernServlet");
-    Loader loader = new SimpleLoader();
+	public static void main(String[] args) {
+		Connector connector = new HttpConnector();
 
-    Context context = new SimpleContext();
-    context.addChild(wrapper1);
-    context.addChild(wrapper2);
+		Wrapper wrapper1 = new SimpleWrapper();
+		wrapper1.setName("Primitive");
+		wrapper1.setServletClass("PrimitiveServlet");
 
-    Mapper mapper = new SimpleContextMapper();
-    mapper.setProtocol("http");
-    
-    LifecycleListener listener = new SimpleContextLifecycleListener();
-    ((Lifecycle) context).addLifecycleListener(listener);
-    
-    context.addMapper(mapper);
-    context.setLoader(loader);
-    
-    // context.addServletMapping(pattern, name);
-    context.addServletMapping("/Primitive", "Primitive");
-    context.addServletMapping("/Modern", "Modern");
+		Wrapper wrapper2 = new SimpleWrapper();
+		wrapper2.setName("Modern");
+		wrapper2.setServletClass("ModernServlet");
+		Loader loader = new SimpleLoader();
 
-    // ------ add logger --------
-    System.setProperty("catalina.base", System.getProperty("user.dir"));
-    FileLogger logger = new FileLogger();
-    logger.setPrefix("FileLog_");
-    logger.setSuffix(".txt");
-    logger.setTimestamp(true);
-    logger.setDirectory("webroot");
-    context.setLogger(logger);
+		Context context = new SimpleContext();
+		context.addChild(wrapper1);
+		context.addChild(wrapper2);
 
-    //---------------------------
+		Mapper mapper = new SimpleContextMapper();
+		mapper.setProtocol("http");
 
-    connector.setContainer(context);
-    try {
-      connector.initialize();
-      ((Lifecycle) connector).start();
-      ((Lifecycle) context).start();
+		LifecycleListener listener = new SimpleContextLifecycleListener();
+		((Lifecycle) context).addLifecycleListener(listener);
 
-      // make the application wait until we press a key.
-      System.in.read();
-      ((Lifecycle) context).stop();
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+		context.addMapper(mapper);
+		context.setLoader(loader);
+
+		// context.addServletMapping(pattern, name);
+		context.addServletMapping("/Primitive", "Primitive");
+		context.addServletMapping("/Modern", "Modern");
+
+		// ------ add logger --------
+		System.setProperty("catalina.base", System.getProperty("user.dir"));
+		FileLogger logger = new FileLogger();
+		logger.setPrefix("FileLog_");
+		logger.setSuffix(".txt");
+		logger.setTimestamp(true);
+		logger.setDirectory("webroot");
+		context.setLogger(logger);
+
+		// ---------------------------
+
+		connector.setContainer(context);
+		try {
+			connector.initialize();
+			((Lifecycle) connector).start();
+			((Lifecycle) context).start();
+
+			// make the application wait until we press a key.
+			System.in.read();
+			((Lifecycle) context).stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
